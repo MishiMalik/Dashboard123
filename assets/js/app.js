e43316c208023bf2f6633adb01f7dd9081c12118
Mehwish <mehwishgul715@gmail.com>
2023-03-17 13:57:29 +0500

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,18 @@
+// =====================================================top btn
+$(window).on('scroll', function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > 50) {
+        $('.go-top').css("display","block");
+    }
+    else {
+      $('.go-top').css("display","none");
+    }
+});
+// ===========================================================sidebar
+
 //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
-
-// for (i = 0; i < dropdown.length; i++) {
-//   dropdown[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var dropdownContent = this.nextElementSibling;
-//     if (dropdownContent.style.display === "flex") {
-//       dropdownContent.style.display = "none";
-
-
-//     } else {
-//       dropdownContent.style.display = "flex";
-      
-//     }
-//   });
-// }
-
 
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
